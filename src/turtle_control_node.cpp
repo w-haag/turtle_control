0192aa3711c947a7b6ff893962cccacf8beca9f9
Christian Pfitzner <christian.pfitzner@t-online.de>
2020-04-16 13:36:18 +0200

    -- update for course at thi

--- src/turtle_control_node.cpp
+++ src/turtle_control_node.cpp
@@ -6,13 +6,15 @@
 #include <geometry_msgs/Twist.h>        // Nachricht zum Verschicken von Geschwindigkeitsbefehlen
 #include <turtlesim/Spawn.h>            // Spawn Service
 #include <turtlesim/Pose.h>             // Nachricht für die Pose der Schildkröte
-#include <turtlesim/Kill.h>
 #include <turtlesim/SetPen.h>
 
 
 void callbackPose(const turtlesim::Pose& msg)
 {
-   // ... 
+  // Achtung Zusatzaufgabe: 
+  // FÜLLEN SIE HIER DAS TEMPLATE MIT EIGENEM CODE
+
+
 }
 
 
@@ -44,21 +46,34 @@ int main(int argc, char **argv)
   printf("Publisher: %s\n", pubname);
 
 
-char penname[64]; 
-sprintf(penname, "%s/%s", argv[1], "set_pen"); 
-  /**
-   * Implementieren Sie hier den Aufruf des Dienstes spawn
-   */
-   ros::ServiceClient client      = n.serviceClient<turtlesim::Spawn>("spawn"); 
-   ros::ServiceClient kill_client = n.serviceClient<turtlesim::Kill>("kill");    
+  char penname[64]; 
+  sprintf(penname, "%s/%s", argv[1], "set_pen"); 
+
+   ros::ServiceClient spawn_client= n.serviceClient<turtlesim::Spawn>("spawn"); 
    ros::ServiceClient pen_client  = n.serviceClient<turtlesim::SetPen>(penname);   
 /*
-    * Senden Sie einen Service-Call über den Service client ab
-    * Beachten Sie, dass der name der Schildkröte mit argv[1] gesetzt wird, so dass
-    * jede Schildkröte im Netzwerk nur einmal vorkommt.
-    */
+
+  /*
+   * Implementieren Sie hier den Aufruf des Dienstes spawn. Hierzu müssen
+   * Sie die Variable srv mit den notwendigen Informationen befüllen. 
+   * Die Definition des Service Spawn haben Sie in der Vorlesung bereits kennen
+   * gelernt. Alternativ dazu können Sie hier auch nochmal nachschlagen: 
+   * http://docs.ros.org/melodic/api/turtlesim/html/srv/Spawn.html
+   *
+   * Senden Sie einen Service-Call über den Service spawn_client ab
+   * Beachten Sie, dass der name der Schildkröte mit argv[1] gesetzt wird, so dass
+   * jede Schildkröte im Netzwerk nur einmal vorkommt.
+   */
    turtlesim::Spawn srv;
-   // ... 
+   // FÜLLEN SIE HIER DAS TEMPLATE MIT EIGENEM CODE
+
+
+
+
+
+
+
+
 
 
    /* Für Schnelle:
@@ -77,7 +92,6 @@ sprintf(penname, "%s/%s", argv[1], "set_pen");
    * zur Steuerung der Schildkröte
    */
    ros::Publisher vel_pub = n.advertise<geometry_msgs::Twist>(pubname, 1); 
-
    ros::Rate r(10); 
 
 
@@ -86,12 +100,24 @@ sprintf(penname, "%s/%s", argv[1], "set_pen");
    {
       /*
        * Erstellen Sie an dieser Stelle eine Nachricht vom Typ geometry_msgs::Twist
+       * Wie die Nachricht definiert ist wissen Sie bereits aus einem vorangeganenen
+       * Versuch mit Turtlesim. Außerdem können Sie hier nochmal nachsehen: 
+       * http://docs.ros.org/melodic/api/geometry_msgs/html/msg/Twist.html
+       *
        * Füllen Sie den Inhalt der Nachricht, so dass die Schildkröte eine Kreis-
-       * bahn fährt.
-       * Schicken Sie die Nachricht
+       * bahn fährt. Schicken Sie die Nachricht im Anschluss mittels des Publishers
+       * vel_pub
        */
       geometry_msgs::Twist msg; 
-      // ... 
+      // FÜLLEN SIE HIER DAS TEMPLATE MIT EIGENEM CODE
+
+
+
+
+
+
+
+
 
       ros::spinOnce();  
       r.sleep(); 
